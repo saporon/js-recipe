7fff05734e3aa8489ea5eb29e4f236af8c6d0806
saporon <tubame_ht-n7-96th@eis.hokudai.ac.jp>
2022-10-09 02:33:18 +0900

    [□と○]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,9 +1,21 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const mainusButton = document.getElementById("mainus-button")
+const dubleButton = document.getElementById("duble-button")
 
 let count = 0
 
 plusButton.onclick = function () {
   count += 1
+  display.textContent = count
+}
+
+mainusButton.onclick = function () {
+  count -= 1
+  display.textContent = count
+}
+
+dubleButton.onclick = function () {
+  count *= 2
   display.textContent = count
 }
